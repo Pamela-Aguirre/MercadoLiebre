82a3a3874ecf6bbd8881b71aa5b5b114b766c0aa
nancypamelaaguirre.770a@fadu.uba.ar <nancypamelaaguirre.770@fadu.uba.ar>
2023-05-22 14:48:26 -0300

    nueva actualizacion

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+//Configuraciones
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    const ruta = path.join(__dirname + "./views/home.html");
+res.sendFile(ruta);
+});
+
+app.listen(3000, () => {
+    console.log("Servidor escuchando en el puerto 3000");
+});
