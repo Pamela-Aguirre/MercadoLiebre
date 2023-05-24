f76e6318972ef13c38997f9a3e4dda8dae31269a
nancypamelaaguirre.770a@fadu.uba.ar <nancypamelaaguirre.770@fadu.uba.ar>
2023-05-24 20:26:55 -0300

    color y alineacion

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    const ruta = path.join(__dirname + "./views/home.html");
+    const ruta = path.join(__dirname + "/views/home.html");
 res.sendFile(ruta);
 });
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en el puerto 3000");
 });
+app.get("/",(req,res)=>{
+
+    res.sendFile(paht.join(__dirname + "./views/home.html"));
+}); 
